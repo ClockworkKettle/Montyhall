7ecded7647513055db76815bec2680613a3347f4
Timothy McNamara <timothyjmcnamara@gmail.com>
2021-02-14 18:04:57 +0000

    Began work on player class

--- montyhalll.cpp
+++ montyhalll.cpp
@@ -3,13 +3,45 @@
 #include <time.h>
 #include <iostream>
 #include <random>
+
+
+class Player
+{
+    int choice;
+public:
+    Player();
+    int getChoice();
+    void choose();
+};
+Player::Player()
+{
+    int choice = NULL;
+}
+int Player::getChoice()
+{
+    return choice;
+}
+void Player::choose()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 3);
+    int random = distrib(gen);
+    random = random-1;
+    choice = random;
+    std::cout << "Player chooses: " << choice << std::endl;
+}
+
 class game {
     bool doors[3] = { };
+    int openeddoor;
 
 public:
     game();
     bool isacar(int i);
     void printdoors();
+    void opendoor(Player player);
+    int getOpenedDoor();
     void choose(int i);
     void changechoice(int i);
 };
@@ -18,13 +50,13 @@ game::game()
     std::random_device rd;  
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(0, 2);
-    int random = distrib(gen);
-    std::cout << "Random num: " << random << std::endl;
+    int random_index = distrib(gen);
+    std::cout << "Random num: " << random_index << std::endl;
     for (int i = 0; i < 3; i++)
     {
         doors[i] = false;
     }
-    doors[random] = true;
+    doors[random_index] = true;
 }
 bool game::isacar(int i)
 {
@@ -40,8 +72,29 @@ void game::printdoors() {
         std::cout << "Car behind door " << i << ": " << doors[i] <<std::endl;
     }
 }
+void game::opendoor(Player player)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if ((doors[i] != true) || (i != player.getChoice()))
+        {
+            openeddoor = i;
+            std::cout << "Door: " << i << " opened" << std::endl;
+        }
+    }
+}
+int game::getOpenedDoor()
+{
+    return openeddoor;
+}
+
+
+
 int main()
 {
     game monty;
     monty.printdoors();
+    Player p;
+    p.choose();
+
 }
